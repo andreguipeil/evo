d114d228da0aa9a61ebe86ece230de43d6feda8a
André Peil <andreguipeil@gail.com>
2015-06-20 21:46:21 -0300

    subindo sparql definitivo

--- app/app/controllers/oficina_controller.rb
+++ app/app/controllers/oficina_controller.rb
@@ -14,29 +14,32 @@ class OficinaController < ApplicationController
 
 respond_to :html, :json, :js
 
-
-
-
 	def index
 		query="
-			SELECT distinct ?refBy ?id ?nameArticle
-			FROM <http://gaci.edu.br>
-			WHERE{
-			   ?article a bibo:AcademicArticle .
-			   ?article dc:title ?nameArticle .
-			   ?article dcterms:isReferencedBy ?refBy .
-			   ?article vivo:relatedBy ?nodeAuthor.
-			   ?article dcterms:issued ?year .
-			   ?nodeAuthor vivo:relates ?nodeAuthor2.
-			   ?nodeAuthor vivo:rank ?rank .
-			   ?nodeAuthor2 rdfs:label ?name .
-			   ?refBy dc:creator ?personCreator.
-			   ?personCreator rdfs:label ?nome.
-			   ?article bibo:presentedAt ?conference.
-			   ?conference dc:title ?nameConference .
-                           ?refBy bibo:identifier ?id.
-                                  FILTER (!regex(str(?nodeAuthor2), concat(\"#author-\",str(?id))))
-			} ORDER BY ?refBy"
+			SELECT DISTINCT  ?refBy ?nameReal ?nameRight (xsd:integer(?rank)) as ?rank ?article ?nameArticle ?nameConference
+				FROM <http://laburb.com>
+				WHERE{
+				 ?article a bibo:AcademicArticle .
+				 ?article dc:title ?nameArticle .
+				 ?article dcterms:isReferencedBy ?refBy .
+				 ?article vivo:relatedBy ?nodeAuthor .
+				 ?article dcterms:issued ?year .
+				 ?article bibo:presentedAt ?conference .
+				 ?conference dc:title ?nameConference .
+
+				 ?nodeAuthor vivo:relates ?nodeAuthor2 .
+				 ?nodeAuthor vivo:rank ?rank .
+				 ?nodeAuthor2 rdfs:label ?nameRight .
+
+				 ?refBy bibo:identifier ?id .
+				 ?refBy dc:creator ?personCreator .
+				 ?personCreator rdfs:label ?nameWrong .
+				 ?personCreator obo:ARG_2000028 ?nodeName .
+				 ?nodeName vcard:hasName ?nameName .
+				 ?nameName vcard:fn ?nameReal .
+				 FILTER (!regex(str(?nodeAuthor2), concat(\"#author-\",str(?id)))).
+				 FILTER (str(?nameReal) = str(?nameWrong))
+			} ORDER BY ?refBy ?nameArticle ?rank"
 		c=ConnectionSPARQL.new
 		data = c.runQuery(query)
 
@@ -44,7 +47,7 @@ respond_to :html, :json, :js
 		#data = data.force_encoding("UTF-8")
 		#logger.info data
 		first, *rest = query.split(/FROM/)
-		cont = first.scan("?").count
+		cont = first.scan("?").count-1
 		triples = csvToArray2(data, cont)
  		@ret = Hash.new
  		#@ret["triples"] = @triples
@@ -94,11 +97,58 @@ respond_to :html, :json, :js
 	return triples
 	end
 
+#######################################################
+# retira todos os conectivos(stopwords) do título do artigo
+# --> Entrada: string
+# --> Saida: string
+#######################################################
+	def retireConetives (article)
+		article.sub('a', '')
+		article.sub('as', '')
+		article.sub('às', '')
+		article.sub('ás', '')
+		article.sub('aos', '')
+		article.sub('com', '')
+		article.sub('como', '')
+		article.sub('cada', '')
+		article.sub('da', '')
+		article.sub('de', '')
+		article.sub('do', '')
+		article.sub('das', '')
+		article.sub('dos', '')
+		article.sub('e', '')
+		article.sub('é', '')
+		article.sub('este', '')
+		article.sub('esta', '')
+		article.sub('em', '')
+		article.sub('faz', '')
+		article.sub('fez', '')
+		article.sub('foi', '')
+		article.sub('fui', '')
+		article.sub('isto', '')
+		article.sub('mesmo', '')
+		article.sub('nós', '')
+		article.sub('não', '')
 
+		article.sub('há', '')
+		article.sub('já', '')
+		article.sub('ja', '')
 
+		article.sub('mas', '')
+		article.sub('muito', '')
+		article.sub('muitos', '')
+		article.sub('mais', '')
 
+		article.sub('ou', '')
+		article.sub('uma', '')
+		article.sub('um', '')
+		article.sub('sao', '')
 
+		article.sub('os', '')
+		article.sub('se', '')
+		article.sub('so', '')
+		article.sub('sua', '')
 
-
+	end
 
 end
